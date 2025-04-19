3a8ce2953ae12ededc02e22b169d33d0854541fb
Testing123453 <wwoods1866@gmail.com>
2025-04-19 17:08:55 -0500

    Update order.php

--- order.php
+++ order.php
@@ -2,32 +2,32 @@
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en">        //defining language (english)
 
 <head> 
-    <meta charset="UTF-8">
-    <meta name="viewpoint" content= "width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">        //setting character encoding to UTF-8
+    <meta name="viewpoint" content= "width=device-width, initial-scale=1.0">        //setting the viewport to ensure the website is responsive on mobile devices
 
-    <title>Thank You For Choosing EncryptedStack!</title>
-    <link rel="stylesheet" href="styles.css">
+    <title>Thank You For Choosing EncryptedStack!</title>        //title of the page shown in the encryption stack
+    <link rel="stylesheet" href="styles.css">        //link to external CSS file we created for styling
 </head>
 
 <body>
     <nav>
-        <div class="logo">TheEncryptedStack</div>
+        <div class="logo">TheEncryptedStack</div>        //adding logo/brand name
 
-        <div class="nav-links">
-            <a href="index.php">Home</a>
-            <a href="#">Contact</a>
+        <div class="nav-links">        //navigation links
+            <a href="index.php">Home</a>        //links back to the home page
+            <a href="#">Contact</a>        //acts as a placeholder link for the contact page
         </div>
 </nav>
 
 <main>
-    <h1>Order Successful</h1>
-    <p>Your payment was successful.</p>
+    <h1>Order Successful</h1>        //heading to indicate the order was successful
+    <p>Your payment was successful.</p>      
 
     <a href="index.php">
-        <button type="button">Continue Browsing</button>
+        <button type="button">Continue Browsing</button>        //button to return to the homepage or to continue browsing
     </a>
 </main>
 
