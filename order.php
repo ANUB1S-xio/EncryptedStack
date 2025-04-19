f757e3e15f4d18f12ba139ea0b88842d253b2d74
Tanner L <140106289+ANUB1S-xio@users.noreply.github.com>
2025-04-19 17:00:21 -0500

    order page

---
+++ order.php
@@ -0,0 +1,35 @@
+<?php require_once('init.php') ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewpoint" content= "width=device-width, initial-scale=1.0">
+
+    <title>Thank You For Choosing EncryptedStack!</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <nav>
+        <div class="logo">TheEncryptedStack</div>
+
+        <div class="nav-links">
+            <a href="index.php">Home</a>
+            <a href="#">Contact</a>
+        </div>
+</nav>
+
+<main>
+    <h1>Order Successful</h1>
+    <p>Your payment was successful.</p>
+
+    <a href="index.php">
+        <button type="button">Continue Browsing</button>
+    </a>
+</main>
+
+</body>
+</html>
