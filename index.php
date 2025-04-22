4393430ece3de742dc45d95a696e9afa64fc1711
Tanner L <140106289+ANUB1S-xio@users.noreply.github.com>
2025-04-22 18:32:00 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   //created variable to store user-facing message regarding added items
   $message = null;        
     //google books API
-  $apiKey = 'AIzaSyCbEelNoTTRxBJXGQOO-LflE_K-V94clrc';
+  $apiKey = 'AIzaSyDOQBJkg0s-IiuupraVuhIg_YXngwYmpa4';
 
   //handling POST requests for adding a book to the cart
   if ($_SERVER['REQUEST_METHOD'] === 'POST') {  
